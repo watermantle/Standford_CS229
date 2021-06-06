3b3011697f05da680f5f416a61967a2a1c7ae19b
watermantle <baiyucqx@gmail.com>
2021-06-06 18:26:31 -0400

    Set up new files

---
+++ Problem Set 1/code/src/run.py
@@ -0,0 +1,6 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('p_num', nargs='?', type=int, default=0,
+                    help='Problem number to run, 0 for all problems.')
+args = parser.parse_args()
