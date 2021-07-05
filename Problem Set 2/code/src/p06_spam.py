3eb568d8ff407c8c364003153703198087ceee33
watermantle <baiyucqx@gmail.com>
2021-07-05 17:47:20 -0400

    create svm and spam files

---
+++ Problem Set 2/code/src/p06_spam.py
@@ -0,0 +1,142 @@
+import collections
+
+import numpy as np
+
+import util
+import svm
+
+def get_words(message):
+    """Get the normalized list of words from a message string.
+
+    This function should split a message into words, normalize them, and return
+    the resulting list. For splitting, you should split on spaces. For normalization,
+    you should convert everything to lowercase.
+
+    Args:
+        message: A string containing an SMS message
+
+    Returns:
+       The list of normalized words from the message.
+    """
+
+    # *** START CODE HERE ***
+    
+    # *** END CODE HERE ***
+
+def create_dictionary(messages):
+    """Create a dictionary mapping words to integer indices.
+
+    This function should create a dictionary of word to indices using the provided
+    training messages. Use get_words to process each message. 
+
+    Rare words are often not useful for modeling. Please only add words to the dictionary
+    if they occur in at least five messages.
+
+    Args:
+        messages: A list of strings containing SMS messages
+
+    Returns:
+        A python dict mapping words to integers.
+    """
+
+    # *** START CODE HERE ***
+
+    # *** END CODE HERE ***
+
+def transform_text(messages, word_dictionary):
+    """Transform a list of text messages into a numpy array for further processing.
+
+    This function should create a numpy array that contains the number of times each word
+    appears in each message. Each row in the resulting array should correspond to each 
+    message and each column should correspond to a word.
+
+    Use the provided word dictionary to map words to column indices. Ignore words that 
+    are not present in the dictionary. Use get_words to get the words for a message.
+
+    Args:
+        messages: A list of strings where each string is an SMS message.
+        word_dictionary: A python dict mapping words to integers.
+
+    Returns:
+        A numpy array marking the words present in each message.
+    """
+    # *** START CODE HERE ***
+
+    # *** END CODE HERE ***
+
+def fit_naive_bayes_model(matrix, labels):
+    """Fit a naive bayes model.
+
+    This function should fit a Naive Bayes model given a training matrix and labels.
+
+    The function should return the state of that model.
+
+    Feel free to use whatever datatype you wish for the state of the model.
+
+    Args:
+        matrix: A numpy array containing word counts for the training data
+        labels: The binary (0 or 1) labels for that training data
+
+    Returns: The trained model
+    """
+
+    # *** START CODE HERE ***
+    
+    # *** END CODE HERE ***
+
+def predict_from_naive_bayes_model(model, matrix):
+    """Use a Naive Bayes model to compute predictions for a target matrix.
+
+    This function should be able to predict on the models that fit_naive_bayes_model
+    outputs.
+
+    Args:
+        model: A trained model from fit_naive_bayes_model
+        matrix: A numpy array containing word counts
+
+    Returns: A numpy array containg the predictions from the model
+    """
+    # *** START CODE HERE ***
+    # *** END CODE HERE ***
+
+def get_top_five_naive_bayes_words(model, dictionary):
+    """Compute the top five words that are most indicative of the spam (i.e positive) class.
+
+    Ues the metric given in 6c as a measure of how indicative a word is.
+    Return the words in sorted form, with the most indicative word first.
+
+    Args:
+        model: The Naive Bayes model returned from fit_naive_bayes_model
+        dictionary: A mapping of word to integer ids
+
+    Returns: The list of the top five most indicative words in sorted order with the most indicative first
+    """
+    # *** START CODE HERE ***
+   
+    # *** END CODE HERE ***
+
+def compute_best_svm_radius(train_matrix, train_labels, val_matrix, val_labels, radius_to_consider):
+    """Compute the optimal SVM radius using the provided training and evaluation datasets.
+
+    You should only consider radius values within the radius_to_consider list.
+    You should use accuracy as a metric for comparing the different radius values.
+
+    Args:
+        train_matrix: The word counts for the training data
+        train_labels: The spma or not spam labels for the training data
+        eval_matrix: The word counts for the validation data
+        eval_labels: The spam or not spam labels for the validation data
+        radius_to_consider: The radius values to consider
+    
+    Returns:
+        The best radius which maximizes SVM accuracy.
+    """
+    # *** START CODE HERE ***
+    
+    # *** END CODE HERE ***
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
